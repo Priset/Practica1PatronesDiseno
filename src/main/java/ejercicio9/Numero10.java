8c24d9ca2fb105c775159491231ff336c6efbdf8
Emanuel Lozano <90929324+Priset@users.noreply.github.com>
2023-02-22 00:30:38 -0400

    Commit at 00:30 of 22/02/2023

---
+++ src/main/java/ejercicio9/Numero10.java
@@ -0,0 +1,11 @@
+package ejercicio9;
+
+public class Numero10 extends SintaxisRomanos{
+    @Override
+    public void interpreter(Context context) {
+        if (context.getInput().startsWith("10")) {
+            context.setOutput(context.getOutput() + "X ");
+            context.setInput(context.getInput().substring(2));
+        }
+    }
+}
---
+++ src/main/java/ejercicio9/SintaxisRomanos.java
@@ -0,0 +1,5 @@
+package ejercicio9;
+
+public abstract class SintaxisRomanos {
+    public abstract void interpreter(Context context);
+}
